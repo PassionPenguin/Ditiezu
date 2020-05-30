97d8a3f1278b5c770599fce2f314ec820e584c0e
PassionPenguin <penguin.zhang@qq.com>
2020-05-30 14:31:30 +0800

    Updated httpExt

--- app/src/main/java/com/passionpenguin/ditiezu/helper/HttpExt.kt
+++ app/src/main/java/com/passionpenguin/ditiezu/helper/HttpExt.kt
@@ -7,6 +7,7 @@ import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLConnection
+import java.nio.charset.Charset
 
 class HttpExt {
     fun retrievePage(url: String, then: (res: String) -> Unit) {
@@ -123,6 +124,76 @@ class HttpExt {
                 "Proxy-Connection",
                 "keep-alive"
             )
+
+            try {
+                val inputStream: InputStream = urlConnection.inputStream
+                val reader = InputStreamReader(inputStream, "GBK")
+                var str = reader.readText()
+                var res = ""
+                while (str != "") {
+                    res += str
+                    str = reader.readText()
+                }
+                result = res;
+            } catch (e: Exception) {
+                result = "Failed Retrieved"
+                Log.i("HttpExt Exception", e.toString())
+            } finally {
+                urlConnection.disconnect()
+            }
+        };
+        thread.start()
+        thread.join()
+        return result
+    }
+
+    fun asyncPostPage(url: String, params: String): String {
+        val urlConnection = URL(url).openConnection() as HttpURLConnection
+        val cookieManager = CookieManager.getInstance()
+        var cookie = cookieManager.getCookie(url)
+
+        var result = ""
+
+        val thread = Thread {
+            if (cookie == null)
+                cookie = ""
+
+            urlConnection.setRequestProperty(
+                "Cookie",
+                cookie
+            )
+
+            urlConnection.requestMethod = "POST"
+            urlConnection.setRequestProperty(
+                "Referer",
+                "http://www.ditiezu.com"
+            )
+            urlConnection.setRequestProperty(
+                "Content-Type",
+                "application/x-www-form-urlencoded;"
+            )
+            urlConnection.setRequestProperty(
+                "Origin",
+                "http://www.ditiezu.com"
+            )
+            urlConnection.setRequestProperty(
+                "Host",
+                "www.ditiezu.com"
+            )
+            urlConnection.setRequestProperty(
+                "User-Agent",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
+            )
+            urlConnection.setRequestProperty(
+                "DNT",
+                "1"
+            )
+            urlConnection.setRequestProperty(
+                "Proxy-Connection",
+                "keep-alive"
+            )
+            urlConnection.doOutput = true
+            urlConnection.outputStream.write(params.toByteArray(Charset.forName("GBK")))
 
             try {
                 val inputStream: InputStream = urlConnection.inputStream
