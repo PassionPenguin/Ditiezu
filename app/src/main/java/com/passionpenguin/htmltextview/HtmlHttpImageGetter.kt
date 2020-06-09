260aa7d461b92188f8af6716c51ee4c60d14e093
PassionPenguin <penguin.zhang@qq.com>
2020-06-09 21:58:33 +0800

    Converted

--- app/src/main/java/com/passionpenguin/htmltextview/HtmlHttpImageGetter.kt
+++ app/src/main/java/com/passionpenguin/htmltextview/HtmlHttpImageGetter.kt
@@ -14,240 +14,208 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.passionpenguin.htmltextview
 
-package com.passionpenguin.htmltextview;
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.os.AsyncTask
+import android.text.Html.ImageGetter
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.lang.ref.WeakReference
+import java.net.URI
+import java.net.URL
 
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
-import android.text.Html.ImageGetter;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
+class HtmlHttpImageGetter(textView: TextView) : ImageGetter {
+    private var container: TextView = textView
+    private var baseUri: URI? = null
+    private var matchParentWidth = false
+    private var compressImage = false
+    private var qualityImage = 50
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.ref.WeakReference;
-import java.net.URI;
-import java.net.URL;
-
-public class HtmlHttpImageGetter implements ImageGetter {
-    private TextView container;
-    private URI baseUri;
-    private boolean matchParentWidth;
-
-    private boolean compressImage = false;
-    private int qualityImage = 50;
-
-    public HtmlHttpImageGetter(TextView textView) {
-        this.container = textView;
-        this.matchParentWidth = false;
+    init {
+        matchParentWidth = false
     }
 
-    public HtmlHttpImageGetter(TextView textView, String baseUrl) {
-        this.container = textView;
-        if (baseUrl != null) {
-            this.baseUri = URI.create(baseUrl);
-        }
-    }
-
-    public HtmlHttpImageGetter(TextView textView, String baseUrl, boolean matchParentWidth) {
-        this.container = textView;
-        this.matchParentWidth = matchParentWidth;
-        if (baseUrl != null) {
-            this.baseUri = URI.create(baseUrl);
-        }
-    }
-
-    public void enableCompressImage(boolean enable) {
-        enableCompressImage(enable, 50);
-    }
-
-    public void enableCompressImage(boolean enable, int quality) {
-        compressImage = enable;
-        qualityImage = quality;
-    }
-
-    public Drawable getDrawable(String source) {
-
-        UrlDrawable urlDrawable = new UrlDrawable();
+    override fun getDrawable(source: String): Drawable {
+        val urlDrawable = UrlDrawable()
 
         // get the actual source
-        ImageGetterAsyncTask asyncTask = new ImageGetterAsyncTask(urlDrawable, this, container, !source.contains("static/image/smiley"), compressImage, qualityImage);
-
-        asyncTask.execute(source);
+        val asyncTask = ImageGetterAsyncTask(
+            urlDrawable,
+            this,
+            container,
+            !source.contains("static/image/smiley"),
+            compressImage,
+            qualityImage
+        )
+        asyncTask.execute(source)
 
         // return reference to URLDrawable which will asynchronously load the image specified in the src tag
-        return urlDrawable;
+        return urlDrawable
     }
 
     /**
-     * Static inner {@link AsyncTask} that keeps a {@link WeakReference} to the {@link UrlDrawable}
-     * and {@link HtmlHttpImageGetter}.
-     * <p/>
+     * Static inner [AsyncTask] that keeps a [WeakReference] to the [UrlDrawable]
+     * and [HtmlHttpImageGetter].
+     *
+     *
      * This way, if the AsyncTask has a longer life span than the UrlDrawable,
      * we won't leak the UrlDrawable or the HtmlRemoteImageGetter.
      */
-    private static class ImageGetterAsyncTask extends AsyncTask<String, Void, Drawable> {
-        private final WeakReference<UrlDrawable> drawableReference;
-        private final WeakReference<HtmlHttpImageGetter> imageGetterReference;
-        private final WeakReference<View> containerReference;
-        private final WeakReference<Resources> resources;
-        private String source;
-        private boolean matchParentWidth;
-        private float scale;
-
-        private boolean compressImage = false;
-        private int qualityImage = 50;
-
-        public ImageGetterAsyncTask(UrlDrawable d, HtmlHttpImageGetter imageGetter, View container,
-                                    boolean matchParentWidth, boolean compressImage, int qualityImage) {
-            this.drawableReference = new WeakReference<>(d);
-            this.imageGetterReference = new WeakReference<>(imageGetter);
-            this.containerReference = new WeakReference<>(container);
-            this.resources = new WeakReference<>(container.getResources());
-            this.matchParentWidth = matchParentWidth;
-            this.compressImage = compressImage;
-            this.qualityImage = qualityImage;
-        }
-
-        @Override
-        protected Drawable doInBackground(String... params) {
-            source = params[0];
-
-            if (resources.get() != null) {
+    private class ImageGetterAsyncTask(
+        d: UrlDrawable, imageGetter: HtmlHttpImageGetter, container: View,
+        private val matchParentWidth: Boolean, compressImage: Boolean, qualityImage: Int
+    ) : AsyncTask<String?, Void?, Drawable?>() {
+        private val drawableReference: WeakReference<UrlDrawable> = WeakReference(d)
+        private val imageGetterReference: WeakReference<HtmlHttpImageGetter> = WeakReference(imageGetter)
+        private val containerReference: WeakReference<View> = WeakReference(container)
+        private val resources: WeakReference<Resources?> = WeakReference(container.resources)
+        private var source: String? = null
+        private var scale = 0f
+        private var compressImage = false
+        private var qualityImage = 50
+        override fun doInBackground(vararg params: String?): Drawable? {
+            source = params[0]
+            return if (resources.get() != null) {
                 if (compressImage) {
-                    return fetchCompressedDrawable(resources.get(), source);
+                    fetchCompressedDrawable(resources.get(), source)
                 } else {
-                    return fetchDrawable(resources.get(), source);
+                    fetchDrawable(resources.get(), source)
                 }
-            }
-
-            return null;
+            } else null
         }
 
-        @Override
-        protected void onPostExecute(Drawable result) {
+        override fun onPostExecute(result: Drawable?) {
             if (result == null) {
-                Log.w(HtmlTextView.TAG, "Drawable result is null! (source: " + source + ")");
-                return;
+                Log.w(
+                    HtmlTextView.TAG,
+                    "Drawable result is null! (source: $source)"
+                )
+                return
             }
-            final UrlDrawable urlDrawable = drawableReference.get();
-            if (urlDrawable == null) {
-                return;
-            }
+            val urlDrawable = drawableReference.get() ?: return
             // set the correct bound according to the result from HTTP call
-            urlDrawable.setBounds(0, 0, (int) (result.getIntrinsicWidth() * scale), (int) (result.getIntrinsicHeight() * scale));
+            urlDrawable.setBounds(
+                0,
+                0,
+                (result.intrinsicWidth * scale).toInt(),
+                (result.intrinsicHeight * scale).toInt()
+            )
 
             // change the reference of the current drawable to the result from the HTTP call
-            urlDrawable.drawable = result;
-
-            final HtmlHttpImageGetter imageGetter = imageGetterReference.get();
-            if (imageGetter == null) {
-                return;
-            }
+            urlDrawable.drawable = result
+            val imageGetter = imageGetterReference.get() ?: return
             // redraw the image by invalidating the container
-            imageGetter.container.invalidate();
+            imageGetter.container.invalidate()
             // re-set text to fix images overlapping text
-            imageGetter.container.setText(imageGetter.container.getText());
+            imageGetter.container.text = imageGetter.container.text
         }
 
         /**
          * Get the Drawable from URL
          */
-        public Drawable fetchDrawable(Resources res, String urlString) {
-            try {
-                InputStream is = fetch(urlString);
-                Drawable drawable = new BitmapDrawable(res, is);
-                scale = getScale(drawable);
-                drawable.setBounds(0, 0, (int) (drawable.getIntrinsicWidth() * scale), (int) (drawable.getIntrinsicHeight() * scale));
-                return drawable;
-            } catch (Exception e) {
-                return null;
+        fun fetchDrawable(
+            res: Resources?,
+            urlString: String?
+        ): Drawable? {
+            return try {
+                val `is` = fetch(urlString)
+                val drawable: Drawable = BitmapDrawable(res, `is`)
+                scale = getScale(drawable)
+                drawable.setBounds(
+                    0,
+                    0,
+                    (drawable.intrinsicWidth * scale).toInt(),
+                    (drawable.intrinsicHeight * scale).toInt()
+                )
+                drawable
+            } catch (e: Exception) {
+                null
             }
         }
 
         /**
          * Get the compressed image with specific quality from URL
          */
-        public Drawable fetchCompressedDrawable(Resources res, String urlString) {
-            try {
-                InputStream is = fetch(urlString);
-                Bitmap original = new BitmapDrawable(res, is).getBitmap();
-
-                ByteArrayOutputStream out = new ByteArrayOutputStream();
-                original.compress(Bitmap.CompressFormat.JPEG, qualityImage, out);
-                original.recycle();
-                is.close();
-
-                Bitmap decoded = BitmapFactory.decodeStream(new ByteArrayInputStream(out.toByteArray()));
-                out.close();
-
-                scale = getScale(decoded);
-                BitmapDrawable b = new BitmapDrawable(res, decoded);
-
-                b.setBounds(0, 0, (int) (b.getIntrinsicWidth() * scale), (int) (b.getIntrinsicHeight() * scale));
-                return b;
-            } catch (Exception e) {
-                return null;
+        fun fetchCompressedDrawable(
+            res: Resources?,
+            urlString: String?
+        ): Drawable? {
+            return try {
+                val `is` = fetch(urlString)
+                val original = BitmapDrawable(res, `is`).bitmap
+                val out = ByteArrayOutputStream()
+                original.compress(Bitmap.CompressFormat.JPEG, qualityImage, out)
+                original.recycle()
+                `is`!!.close()
+                val decoded =
+                    BitmapFactory.decodeStream(ByteArrayInputStream(out.toByteArray()))
+                out.close()
+                scale = getScale(decoded)
+                val b = BitmapDrawable(res, decoded)
+                b.setBounds(
+                    0,
+                    0,
+                    (b.intrinsicWidth * scale).toInt(),
+                    (b.intrinsicHeight * scale).toInt()
+                )
+                b
+            } catch (e: Exception) {
+                null
             }
         }
 
-        private float getScale(Bitmap bitmap) {
-            View container = containerReference.get();
-            if (container == null) {
-                return 1f;
-            }
-
-            float maxWidth = container.getWidth();
-            float originalDrawableWidth = bitmap.getWidth();
-
-            return maxWidth / originalDrawableWidth;
+        private fun getScale(bitmap: Bitmap): Float {
+            val container = containerReference.get() ?: return 1f
+            val maxWidth = container.width.toFloat()
+            val originalDrawableWidth = bitmap.width.toFloat()
+            return maxWidth / originalDrawableWidth
         }
 
-        private float getScale(Drawable drawable) {
-            View container = containerReference.get();
+        private fun getScale(drawable: Drawable): Float {
+            val container = containerReference.get()
             if (!matchParentWidth || container == null) {
-                return 1f;
+                return 1f
             }
-
-            float maxWidth = container.getWidth();
-            float originalDrawableWidth = drawable.getIntrinsicWidth();
-
-            return maxWidth / originalDrawableWidth;
+            val maxWidth = container.width.toFloat()
+            val originalDrawableWidth = drawable.intrinsicWidth.toFloat()
+            return maxWidth / originalDrawableWidth
         }
 
-        private InputStream fetch(String urlString) throws IOException {
-            URL url;
-            final HtmlHttpImageGetter imageGetter = imageGetterReference.get();
-            if (imageGetter == null) {
-                return null;
-            }
-            if (imageGetter.baseUri != null) {
-                url = imageGetter.baseUri.resolve(urlString).toURL();
+        @Throws(IOException::class)
+        private fun fetch(urlString: String?): InputStream? {
+            val url: URL
+            val imageGetter = imageGetterReference.get() ?: return null
+            url = if (imageGetter.baseUri != null) {
+                imageGetter.baseUri!!.resolve(urlString).toURL()
             } else {
-                url = URI.create(urlString).toURL();
+                URI.create(urlString).toURL()
             }
+            return url.content as InputStream
+        }
 
-            return (InputStream) url.getContent();
+        init {
+            this.compressImage = compressImage
+            this.qualityImage = qualityImage
         }
     }
 
-    @SuppressWarnings("deprecation")
-    public class UrlDrawable extends BitmapDrawable {
-        protected Drawable drawable;
-
-        @Override
-        public void draw(Canvas canvas) {
+    inner class UrlDrawable : BitmapDrawable() {
+        var drawable: Drawable? = null
+        override fun draw(canvas: Canvas) {
             // override the draw to facilitate refresh function later
             if (drawable != null) {
-                drawable.draw(canvas);
+                drawable!!.draw(canvas)
             }
         }
     }
