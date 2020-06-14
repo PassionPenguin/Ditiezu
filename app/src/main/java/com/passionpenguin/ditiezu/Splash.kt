aee8fd65a836caa86fc5ff6b4246273206bef09e
PassionPenguin <penguin.zhang@qq.com>
2020-06-14 18:51:43 +0800

    Update Package

--- app/src/main/java/com/passionpenguin/ditiezu/Splash.kt
+++ app/src/main/java/com/passionpenguin/ditiezu/Splash.kt
@@ -7,28 +7,17 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.RequestConfiguration
 import kotlinx.android.synthetic.main.activity_splash.*
 
 
 class Splash : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // Initialize the Mobile Ads SDK.
-        MobileAds.initialize(this) { }
-
-        // Set your test devices. Check your logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
-        // to get test ads on this device."
-        MobileAds.setRequestConfiguration(
-            RequestConfiguration.Builder()
-                .setTestDeviceIds(listOf("4da52e090605"))
-                .build()
-        )
+        MobileAds.initialize(
+            this
+        ) { }
 
         adView.loadAd(AdRequest.Builder().build())
         adView.adListener = object : AdListener() {
