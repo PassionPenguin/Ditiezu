67e39850f8a78f2ab001e6eb924c29ead5b9f2c6
PassionPenguin <penguin.zhang@qq.com>
2020-06-14 15:09:51 +0800

    Import LibPicture(:picture_library), Remove unused methods, Optimize.

---
+++ picture_library/src/main/java/com/luck/picture/lib/tools/DoubleUtils.java
@@ -0,0 +1,26 @@
+package com.luck.picture.lib.tools;
+
+/**
+ * author：luck
+ * project：PictureSelector
+ * package：com.luck.picture.lib.tool
+ * email：893855882@qq.com
+ * data：2017/5/25
+ */
+
+public class DoubleUtils {
+    private final static long TIME = 800;
+    /**
+     * Prevent continuous click, jump two pages
+     */
+    private static long lastClickTime;
+
+    public static boolean isFastDoubleClick() {
+        long time = System.currentTimeMillis();
+        if (time - lastClickTime < TIME) {
+            return true;
+        }
+        lastClickTime = time;
+        return false;
+    }
+}
---
+++ picture_library/src/main/java/com/luck/picture/lib/widget/MScroller.java
@@ -0,0 +1,41 @@
+package com.luck.picture.lib.widget;
+
+import android.content.Context;
+import android.view.animation.Interpolator;
+import android.widget.Scroller;
+
+/**
+ * @author：luck
+ * @date：2020-04-11 14:41
+ * @describe：MScroller
+ */
+public class MScroller extends Scroller {
+
+    private static final Interpolator sInterpolator = t -> {
+        t -= 1.0f;
+        return t * t * t * t * t + 1.0f;
+    };
+    public boolean noDuration;
+
+    public MScroller(Context context) {
+        this(context, sInterpolator);
+    }
+
+    public MScroller(Context context, Interpolator interpolator) {
+        super(context, interpolator);
+    }
+
+    public void setNoDuration(boolean noDuration) {
+        this.noDuration = noDuration;
+    }
+
+    @Override
+    public void startScroll(int startX, int startY, int dx, int dy, int duration) {
+        if (noDuration) {
+            //界面滑动不需要时间间隔
+            super.startScroll(startX, startY, dx, dy, 0);
+        } else {
+            super.startScroll(startX, startY, dx, dy, duration);
+        }
+    }
+}
