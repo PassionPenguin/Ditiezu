5f42c1c2b191bc4c99b430ba41dad320fe517384
霜羽 HoarfrostFeather <penguin.zhang@qq.com>
2020-06-26 10:27:14 +0800

    Update Generated Resources

--- app/src/main/java/com/passionpenguin/ditiezu/Splash.kt
+++ app/src/main/java/com/passionpenguin/ditiezu/Splash.kt
@@ -3,14 +3,11 @@ package com.passionpenguin.ditiezu
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.ads.AdListener
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.MobileAds
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
 import com.tencent.bugly.crashreport.CrashReport
-import kotlinx.android.synthetic.main.activity_splash.*
+import java.util.*
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,26 +20,10 @@ class Splash : AppCompatActivity() {
             Analytics::class.java, Crashes::class.java
         )
 
-        MobileAds.initialize(
-            this
-        ) { }
-
-        adView.loadAd(AdRequest.Builder().build())
-        adView.adListener = object : AdListener() {
-            override fun onAdLoaded() {
-                super.onAdLoaded()
-                adView.loadAd(AdRequest.Builder().build())
-            }
-
-            override fun onAdFailedToLoad(p0: Int) {
-                super.onAdFailedToLoad(p0)
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
                 startActivity(Intent(this@Splash, MainActivity::class.java))
             }
-
-            override fun onAdClosed() {
-                super.onAdClosed()
-                startActivity(Intent(this@Splash, MainActivity::class.java))
-            }
-        }
+        }, 1500)
     }
 }
