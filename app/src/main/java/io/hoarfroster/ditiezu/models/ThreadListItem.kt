a7d3d17601a6f17e78a64827975865503b19da6e
Hoarfroster <hoarfroster@outlook.com>
2021-04-06 03:18:48 +0800

    rm project

--- app/src/main/java/io/hoarfroster/ditiezu/models/ThreadListItem.kt
+++
@@ -1,9 +0,0 @@
-package io.hoarfroster.ditiezu.models
-
-class ThreadListItem(
-    val title: String,
-    val description: String,
-    val userName: String,
-    val uid: Int,
-    val categoryName: String
-)
